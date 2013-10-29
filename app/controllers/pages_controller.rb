77679678f64eb7e122a7c5ac5ee1f9baa55f624b
Daivat Dholakia <daivatddholakia@gmail.com>
2013-10-28 22:18:30 -0700

    bundle install, bootstrap, heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def privacy
+  end  
+  def tos
+  end  
+
+  def contact
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Tips::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "privacy" => "pages#privacy"
+  get "tos" => "pages#tos"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
